72d15d48007dfb2eae877062ab3d1e50736e27f4
just331 <justinr331@gmail.com>
2020-02-05 21:10:02 -0600

    Initial start for project 1.

---
+++ Project1.py
@@ -0,0 +1,9 @@
+def main():
+    s1 = [line.strip() for line in open("source1.txt", "r")]
+    s2 = [line.strip() for line in open("source2.txt", "r")]
+    s3 = [line.strip() for line in open("source3.txt", "r")]
+    s4 = [line.strip() for line in open("source4.txt", "r")]
+    s5 = [line.strip() for line in open("source5.txt", "r")]
+
+
+main()
