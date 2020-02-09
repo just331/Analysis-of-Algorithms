ac0b6a093d968c9ae7f2ec6e35ab10859b4a2fb3
Justin <justinr331@gmail.com>
2020-02-09 16:59:43 -0600

    Reworked formatting for source. Ready for inversion count.

--- Project1.py
+++ Project1.py
@@ -1,6 +1,7 @@
 # Justin Rodriguez
 # CS 5381 Analysis of Algorithms
 # Project 1
+from _ast import operator
 
 
 def combineRank(arr1, arr2, arr3, arr4, arr5):
@@ -17,6 +18,17 @@ def combineRank(arr1, arr2, arr3, arr4, arr5):
     return combarr
 
 
+def formatlist(l1, l2):
+    page = []
+    for x in range(10000):
+        page.append("Page {}".format(x))
+
+    l1 = list(zip(page, l1))
+
+    dic = {v: j for j, v in enumerate(l2)}
+    return sorted(l1, key=lambda p: dic[p[0]])
+
+
 # Need to do merge sort on arrays with inversion count
 # Sort
 
@@ -25,27 +37,31 @@ def combineRank(arr1, arr2, arr3, arr4, arr5):
 # " " algorithm of choice " "
 
 def main():
-    s1 = [line.strip() for line in open("source1.txt", "r")]
+    s1 = [line.strip() for line in open("data/source1.txt", "r")]
     s1 = [int(i) for i in s1]
-    s2 = [line.strip() for line in open("source2.txt", "r")]
+    s2 = [line.strip() for line in open("data/source2.txt", "r")]
     s2 = [int(i) for i in s2]
-    s3 = [line.strip() for line in open("source3.txt", "r")]
+    s3 = [line.strip() for line in open("data/source3.txt", "r")]
     s3 = [int(i) for i in s3]
-    s4 = [line.strip() for line in open("source4.txt", "r")]
+    s4 = [line.strip() for line in open("data/source4.txt", "r")]
     s4 = [int(i) for i in s4]
-    s5 = [line.strip() for line in open("source5.txt", "r")]
+    s5 = [line.strip() for line in open("data/source5.txt", "r")]
     s5 = [int(i) for i in s5]
-    print("The combined rank of each web page is: ", combineRank(s1, s2, s3, s4, s5))
 
-    page = []
-    for x in range(10000):
-        page.append("Page {}".format(x))
+    cr = combineRank(s1, s2, s3, s4, s5)
+    print("The combined rank of each web page is: ", cr)
+
+    crp = [i[0] for i in cr]
+
+    c1 = formatlist(s1, crp)
+    print(c1)
+    c2 = formatlist(s2, crp)
+    c3 = formatlist(s3, crp)
+    c4 = formatlist(s4, crp)
+    c5 = formatlist(s5, crp)
+
     # create list that contains the combined rank list as well as the corresponding page
-    c1 = list(zip(page, s1))
-    c2 = list(zip(page, s2))
-    c3 = list(zip(page, s3))
-    c4 = list(zip(page, s4))
-    c5 = list(zip(page, s5))
+    # c1 = list(zip(page, s1))
 
 
 main()
